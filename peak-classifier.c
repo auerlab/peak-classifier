fb1d2617caa7292a31d093f4048ec0fca2e556ba
Jason Bacon <bacon@moray.acadix.biz>
2021-04-08 13:12:13 -0500

    Generate better test BED, check sort order

--- peak-classifier.c
+++ peak-classifier.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <biostring.h>
 #include "peak-classifier.h"
 
 int     main(int argc,char *argv[])
@@ -66,14 +67,46 @@ int     main(int argc,char *argv[])
 int     classify(FILE *bed_stream, FILE *gff_stream)
 
 {
+    uint64_t    last_pos = 0;
+    char        last_chrom[BED_CHROMOSOME_MAX_CHARS + 1];
     bed_feature_t   bed_feature;
 
     bed_skip_header(bed_stream);
     while ( bed_read_feature(bed_stream, &bed_feature) == BIO_READ_OK )
     {
+	bed_check_order(&bed_feature, last_chrom, last_pos);
 	bed_write_feature(stdout, &bed_feature, BED_FIELD_ALL);
     }
     return EX_OK;
+}
+
+
+/***************************************************************************
+ *  Description:
+ *      Make sure the BED input is sorted
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2021-04-08  Jason Bacon Begin
+ ***************************************************************************/
+
+void    bed_check_order(bed_feature_t *bed_feature, char last_chrom[],
+			uint64_t last_pos)
+
+{
+    if ( chromosome_name_cmp(bed_feature->chromosome, last_chrom) == 0 )
+    {
+	if ( bed_feature->start_pos < last_pos )
+	{
+	    fprintf(stderr, "peak-classifier: BED file not sorted by start position.\n");
+	    exit(EX_DATAERR);
+	}
+    }
+    else if ( chromosome_name_cmp(bed_feature->chromosome, last_chrom) < 0 )
+    {
+	fprintf(stderr, "peak-classifier: BED file not sorted by start chromosome.\n");
+	exit(EX_DATAERR);
+    }
 }
 
 
@@ -83,4 +116,3 @@ void    usage(char *argv[])
     fprintf(stderr, "Usage: %s BED-file GFF-file\n", argv[0]);
     exit(EX_USAGE);
 }
-
--- protos.h
+++ protos.h
@@ -1,4 +1,5 @@
 /* peak-classifier.c */
 int main(int argc, char *argv[]);
 int classify(FILE *bed_stream, FILE *gff_stream);
+void bed_check_order(bed_feature_t *bed_feature, char last_chrom[], uint64_t last_pos);
 void usage(char *argv[]);
